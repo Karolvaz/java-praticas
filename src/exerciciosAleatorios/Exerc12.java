224f999de855fe9c349a17b5c1648d391074bd8a
Karoline <karolinelemesvaz@gmail.com>
2023-06-23 14:15:15 -0300

    Desafio Calculadora

--- src/exerciciosAleatorios/Exerc12.java
+++ src/exerciciosAleatorios/Exerc12.java
@@ -9,7 +9,8 @@ public class Exerc12 {
         double valor2;
         int operador;
         double resultado = 0;
-        int repetir;
+        int repetirTudo;
+
 
         Scanner leitor = new Scanner(System.in);
         System.out.print("Bem-vinda(o) à Karol's Calculator \n");
@@ -47,9 +48,46 @@ public class Exerc12 {
 
         do{
             System.out.println("Calcular novamente? [ 1: Sim ], [ 2 - Não ]");
-            repetir = leitor.nextInt();
+            repetirTudo = leitor.nextInt();
+        } while (repetirTudo == 2);
+
+        System.out.print("Digite o 1º valor: ");
+        valor1 = leitor.nextDouble();
+
+        do {
+            System.out.print("Digite o número correspondente à operação, sendo.. \n " +
+                    "[ 1: + ], [ 2: - ], [ 3: * ], [ 4: / ] : ");
+            operador = leitor.nextInt();
+        } while (operador < 1 || operador > 4);
+        System.out.print("Digite o 2º valor: ");
+        valor2 = leitor.nextInt();
+
+        System.out.println();
+
+        if (operador == 1){
+            resultado = valor1 + valor2;
+            System.out.println("Você informou: " + valor1 + " + " + valor2);
         }
-        while (repetir != 2);
+        if (operador == 2){
+            resultado = valor1 - valor2;
+            System.out.println("Você informou: " + valor1 + " - " + valor2);
+        }
+        if (operador == 3){
+            resultado = valor1 * valor2;
+            System.out.println("Você informou: " + valor1 + " * " + valor2);
+        }
+        if (operador == 4){
+            resultado = valor1 / valor2;
+            System.out.println("Você informou " + valor1 + " / " + valor2);
+        }
+
+        System.out.println("O resultado da Operação é: " + resultado);
+        System.out.println();
+
+        do{
+            System.out.println("Calcular novamente? [ 1: Sim ], [ 2 - Não ]");
+            repetirTudo = leitor.nextInt();
+        } while (repetirTudo != 2);
         System.out.println("Obrigado, até a próxima");
 
         leitor.close();
