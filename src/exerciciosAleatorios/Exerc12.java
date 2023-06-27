f95974df6bbca9554786825511cb9d75dae0f958
Karoline <karolinelemesvaz@gmail.com>
2023-06-27 19:06:26 -0300

    Desafio Calculadora

--- src/exerciciosAleatorios/Exerc12.java
+++ src/exerciciosAleatorios/Exerc12.java
@@ -7,88 +7,97 @@ public class Exerc12 {
 
         double valor1;
         double valor2;
+        int valorOperador;
         int operador;
         double resultado = 0;
-        int repetirTudo;
 
+        double soma;
+        double subtracao;
+        double multiplicacao;
+        double divisao;
+
+        int valorDigitado;
+        boolean repetirTudo;
 
         Scanner leitor = new Scanner(System.in);
         System.out.print("Bem-vinda(o) à Karol's Calculator \n");
-        System.out.println();
-
-        System.out.print("Digite o 1º valor: ");
-        valor1 = leitor.nextDouble();
 
         do {
-            System.out.print("Digite o número correspondente à operação, sendo.. \n " +
-                "[ 1: + ], [ 2: - ], [ 3: * ], [ 4: / ] : ");
-            operador = leitor.nextInt();
-            } while (operador < 1 || operador > 4);
-                System.out.print("Digite o 2º valor: ");
-                valor2 = leitor.nextInt();
+            System.out.print("Digite o 1º valor: ");
+            valor1 = leitor.nextDouble();
 
-        System.out.println();
-
-        if (operador == 1){
-            resultado = valor1 + valor2;
-            System.out.println("Você informou: " + valor1 + " + " + valor2);
-        } if (operador == 2){
-            resultado = valor1 - valor2;
-            System.out.println("Você informou: " + valor1 + " - " + valor2);
-        } if (operador == 3){
-            resultado = valor1 * valor2;
-            System.out.println("Você informou: " + valor1 + " * " + valor2);
-        } if (operador == 4){
-            resultado = valor1 / valor2;
-            System.out.println("Você informou " + valor1 + " / " + valor2);
-        }
-
-        System.out.println("O resultado da Operação é: " + resultado);
-        System.out.println();
-
-        do{
-            System.out.println("Calcular novamente? [ 1: Sim ], [ 2 - Não ]");
-            repetirTudo = leitor.nextInt();
-        } while (repetirTudo == 2);
-
-        System.out.print("Digite o 1º valor: ");
-        valor1 = leitor.nextDouble();
-
-        do {
             System.out.print("Digite o número correspondente à operação, sendo.. \n " +
                     "[ 1: + ], [ 2: - ], [ 3: * ], [ 4: / ] : ");
-            operador = leitor.nextInt();
+            valorOperador = leitor.nextInt();
+            operador = valorOperador;
+
+            System.out.print("Digite o 2º valor: ");
+            valor2 = leitor.nextDouble();
+
+            if (operador == 1) {
+                soma = valor1 + valor2;
+                resultado = soma;
+                System.out.printf("Você informou: %.2f + %.2f.\n", valor1, valor2);
+            }
+            if (operador == 2) {
+                subtracao = valor1 - valor2;
+                resultado = subtracao;
+                System.out.printf("Você informou: %.2f - %.2f.\n", valor1, valor2);
+            }
+            if (operador == 3) {
+                multiplicacao = valor1 * valor2;
+                resultado = multiplicacao;
+                System.out.printf("Você informou: %.2f * %.2f.\n", valor1, valor2);
+            }
+            if (operador == 4) {
+                divisao = valor1 / valor2;
+                resultado = divisao;
+                System.out.printf("Você informou: %.2f /+ %.2f.\n", valor1, valor2);
+            }
+
         } while (operador < 1 || operador > 4);
-        System.out.print("Digite o 2º valor: ");
-        valor2 = leitor.nextInt();
+        System.out.printf("O resultado é %.2f\n", resultado);
 
-        System.out.println();
-
-        if (operador == 1){
-            resultado = valor1 + valor2;
-            System.out.println("Você informou: " + valor1 + " + " + valor2);
-        }
-        if (operador == 2){
-            resultado = valor1 - valor2;
-            System.out.println("Você informou: " + valor1 + " - " + valor2);
-        }
-        if (operador == 3){
-            resultado = valor1 * valor2;
-            System.out.println("Você informou: " + valor1 + " * " + valor2);
-        }
-        if (operador == 4){
-            resultado = valor1 / valor2;
-            System.out.println("Você informou " + valor1 + " / " + valor2);
-        }
-
-        System.out.println("O resultado da Operação é: " + resultado);
-        System.out.println();
 
         do{
-            System.out.println("Calcular novamente? [ 1: Sim ], [ 2 - Não ]");
-            repetirTudo = leitor.nextInt();
-        } while (repetirTudo != 2);
-        System.out.println("Obrigado, até a próxima");
+            System.out.print("Calcular novamente? [ 1: Sim ], [ 2 - Não ]: ");
+            valorDigitado = leitor.nextInt();
+            repetirTudo = (valorDigitado == 1);
+
+             if (repetirTudo){
+                 System.out.print("Digite o 1º valor: ");
+                 valor1 = leitor.nextDouble();
+
+                 System.out.print("Digite o número correspondente à operação, sendo.. \n " +
+                         "[ 1: + ], [ 2: - ], [ 3: * ], [ 4: / ] : ");
+                 operador = leitor.nextInt();
+
+                 System.out.print("Digite o 2º valor: ");
+                 valor2 = leitor.nextDouble();
+
+                 if (operador == 1){
+                     soma = valor1 + valor2;
+                     resultado = soma;
+                     System.out.printf("Você informou: %.2f + %.2f.\n", valor1, valor2);
+                 } if (operador == 2){
+                     subtracao = valor1 - valor2;
+                     resultado = subtracao;
+                     System.out.printf("Você informou: %.2f - %.2f.\n", valor1, valor2);
+                 } if (operador == 3){
+                     multiplicacao = valor1 * valor2;
+                     resultado = multiplicacao;
+                     System.out.printf("Você informou: %.2f * %.2f.\n", valor1, valor2);
+                 } if (operador == 4){
+                     divisao = valor1 / valor2;
+                     resultado = divisao;
+                     System.out.printf("Você informou: %.2f /+ %.2f.\n", valor1, valor2);
+                 }
+                 System.out.println("O resultado da Operação é: " + resultado);
+             }
+              valorDigitado++;
+
+        } while (valorDigitado == 2);
+        System.out.println("Obrigado e até a próxima!");
 
         leitor.close();
     }
